1b1a7cc88569073c69d58cc0a71e35b4b629afc1
Andy Barajas <andress.barajas@gmail.com>
2024-05-09 06:57:56 -0700

    Remove usage of realpath in utils/build-all.sh

--- utils/build-all.sh
+++ utils/build-all.sh
@@ -10,7 +10,7 @@ cd ${KOS_PORTS}
 errors=""
 error_count=0
 
-for _dir in $(realpath ${KOS_PORTS})/* ; do
+for _dir in ${KOS_PORTS}/* ; do
     if [ -d "${_dir}" ] ; then
         if [ -f "${_dir}/Makefile" ] ; then
             echo "Checking if ${_dir} is installed and up-to-date..."
