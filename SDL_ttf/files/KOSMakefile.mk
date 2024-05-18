e8d28a43410f3386c377c56298fad02dd15a3091
Donald Haase <quzar86@yahoo.com>
2024-05-18 00:09:05 -0400

    Remove extraneous lm (#68)
    Co-authored-by: QuzarDC <quzar@completext.com>

--- SDL_ttf/files/KOSMakefile.mk
+++ SDL_ttf/files/KOSMakefile.mk
@@ -5,6 +5,6 @@ KOS_CFLAGS += -I. \
 	-I${KOS_PORTS}/include/SDL -lSDL \
 	-I${KOS_PORTS}/include/zlib -I${KOS_PORTS}/include/bzlib \
 	-I${KOS_PORTS}/include/png -I${KOS_PORTS}/include/freetype2 \
-	-lfreetype -lz -lbz2 -lpng -lm
+	-lfreetype -lz -lbz2 -lpng
 
 include ${KOS_PORTS}/scripts/lib.mk
