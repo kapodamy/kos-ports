6bc2d4faa4c4358b35250473d48cea4e4ea5c3f8
Colton Pawielski <cepawiel@users.noreply.github.com>
2024-06-04 22:11:51 -0500

    Fix libtsunami build on GCC9&10 (#71)

--- libtsunami/files/KOSMakefile.mk
+++ libtsunami/files/KOSMakefile.mk
@@ -5,6 +5,8 @@ OBJS_ANIMS := $(patsubst %.cpp,%.o,$(wildcard src/anims/*.cpp))
 OBJS_TRIGS := $(patsubst %.cpp,%.o,$(wildcard src/triggers/*.cpp))
 OBJS := $(OBJS_MAIN) $(OBJS_DRW) $(OBJS_ANIMS) $(OBJS_TRIGS)
 
-KOS_CFLAGS += -Iinclude
+# GCC versions prior to 11.0 have incomplete C++17 support and need 
+# to be forced to use the GNU dialect for std::filesystem support
+KOS_CPPFLAGS += -Iinclude -std=gnu++17
 
 include ${KOS_PORTS}/scripts/lib.mk
